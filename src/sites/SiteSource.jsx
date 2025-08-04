e98c8ae1c44af101233efb6f393377294dbc853a
Dzarooo <kacperjaroszewski06@gmail.com>
2025-08-04 23:59:15 +0200

    made contact website even though it is named everywhere sitesource

--- src/sites/SiteSource.jsx
+++ src/sites/SiteSource.jsx
@@ -1,7 +1,44 @@
 const SiteSource = () => {
     return (
-        <div className="w-full h-[100vh] flex items-center justify-center">
-            <p>Site source</p>
+        <div className="w-full h-[100vh] flex items-center justify-center flex-col">
+            <div className="border-2 border-slate-100 p-2 rounded-xl flex bg-black/25 w-[1000px] h-[500px]">
+                <div className="flex-1/3 flex justify-center items-center p-2">
+                    <h1 className="text-5xl text-purple-300">Get in touch with me!</h1>
+                </div>
+                <span className="bg-slate-100 w-[1.5px] h-full"></span>
+                <div className="flex-2/3 flex flex-col justify-center gap-2 p-5">
+                    <a href="https://github.com/Dzarooo" target="_blank" className="flex gap-2 items-center group hover:translate-x-2 transition-transform">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 48 48" className="fill-slate-100 group-hover:fill-purple-300 transition-colors">
+                            <path d="M 24 4 C 12.972066 4 4 12.972074 4 24 C 4 35.027926 12.972066 44 24 44 C 35.027934 44 44 35.027926 44 24 C 44 12.972074 35.027934 4 24 4 z M 24 7 C 33.406615 7 41 14.593391 41 24 C 41 31.66536 35.956939 38.122519 29 40.251953 L 29 35.136719 C 29 33.226635 27.899316 31.588619 26.308594 30.773438 A 10 8 0 0 0 32.4375 18.720703 C 32.881044 17.355414 33.376523 14.960672 32.199219 13.076172 C 29.929345 13.076172 28.464667 14.632086 27.765625 15.599609 A 10 8 0 0 0 24 15 A 10 8 0 0 0 20.230469 15.59375 C 19.529731 14.625773 18.066226 13.076172 15.800781 13.076172 C 14.449711 15.238817 15.28492 17.564557 15.732422 18.513672 A 10 8 0 0 0 21.681641 30.779297 C 20.3755 31.452483 19.397283 32.674042 19.097656 34.15625 L 17.783203 34.15625 C 16.486203 34.15625 15.98225 33.629234 15.28125 32.740234 C 14.58925 31.851234 13.845172 31.253859 12.951172 31.005859 C 12.469172 30.954859 12.144453 31.321484 12.564453 31.646484 C 13.983453 32.612484 14.081391 34.193516 14.650391 35.228516 C 15.168391 36.160516 16.229687 37 17.429688 37 L 19 37 L 19 40.251953 C 12.043061 38.122519 7 31.66536 7 24 C 7 14.593391 14.593385 7 24 7 z"></path>
+                        </svg>
+                        <span className="text-slate-100 text-xl group-hover:text-purple-300 transition-colors">https://github.com/Dzarooo</span>
+                    </a>
+                    <a href="#" target="_blank" className="flex gap-2 items-center group hover:translate-x-2 transition-transform">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 24 24" className="fill-slate-100 group-hover:fill-purple-300 transition-colors">
+                            <path d="M 5 3 C 3.895 3 3 3.895 3 5 L 3 19 C 3 20.105 3.895 21 5 21 L 19 21 C 20.105 21 21 20.105 21 19 L 21 5 C 21 3.895 20.105 3 19 3 L 5 3 z M 5 5 L 19 5 L 19 19 L 5 19 L 5 5 z M 7.7792969 6.3164062 C 6.9222969 6.3164062 6.4082031 6.8315781 6.4082031 7.5175781 C 6.4082031 8.2035781 6.9223594 8.7167969 7.6933594 8.7167969 C 8.5503594 8.7167969 9.0644531 8.2035781 9.0644531 7.5175781 C 9.0644531 6.8315781 8.5502969 6.3164062 7.7792969 6.3164062 z M 6.4765625 10 L 6.4765625 17 L 9 17 L 9 10 L 6.4765625 10 z M 11.082031 10 L 11.082031 17 L 13.605469 17 L 13.605469 13.173828 C 13.605469 12.034828 14.418109 11.871094 14.662109 11.871094 C 14.906109 11.871094 15.558594 12.115828 15.558594 13.173828 L 15.558594 17 L 18 17 L 18 13.173828 C 18 10.976828 17.023734 10 15.802734 10 C 14.581734 10 13.930469 10.406562 13.605469 10.976562 L 13.605469 10 L 11.082031 10 z"></path>
+                        </svg>
+                        <span className="text-slate-100 text-xl group-hover:text-purple-300 transition-colors">https://linked.in/Dzarooo</span>
+                    </a>
+                    <a href="https://www.facebook.com/kacper.jaroszewski.7792/" target="_blank" className="flex gap-2 items-center group hover:translate-x-2 transition-transform">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 24 24" className="fill-slate-100 group-hover:fill-purple-300 transition-colors">
+                            <path d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.014467 17.065322 19.313017 13.21875 19.898438 L 13.21875 14.384766 L 15.546875 14.384766 L 15.912109 12.019531 L 13.21875 12.019531 L 13.21875 10.726562 C 13.21875 9.7435625 13.538984 8.8710938 14.458984 8.8710938 L 15.935547 8.8710938 L 15.935547 6.8066406 C 15.675547 6.7716406 15.126844 6.6953125 14.089844 6.6953125 C 11.923844 6.6953125 10.654297 7.8393125 10.654297 10.445312 L 10.654297 12.019531 L 8.4277344 12.019531 L 8.4277344 14.384766 L 10.654297 14.384766 L 10.654297 19.878906 C 6.8702905 19.240845 4 15.970237 4 12 C 4 7.5698774 7.5698774 4 12 4 z"></path>
+                        </svg>
+                        <span className="text-slate-100 text-xl group-hover:text-purple-300 transition-colors">https://www.facebook.com/kacper.jaroszewski.7792/</span>
+                    </a>
+                    <a href="https://www.instagram.com/czare.ek/" target="_blank" className="flex gap-2 items-center group hover:translate-x-2 transition-transform">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 24 24" className="fill-slate-100 group-hover:fill-purple-300 transition-colors">
+                            <path d="M 8 3 C 5.2475888 3 3 5.2475888 3 8 L 3 16 C 3 18.752411 5.2475888 21 8 21 L 16 21 C 18.752411 21 21 18.752411 21 16 L 21 8 C 21 5.2475888 18.752411 3 16 3 L 8 3 z M 8 4.5 L 16 4.5 C 17.941589 4.5 19.5 6.0584112 19.5 8 L 19.5 16 C 19.5 17.941589 17.941589 19.5 16 19.5 L 8 19.5 C 6.0584112 19.5 4.5 17.941589 4.5 16 L 4.5 8 C 4.5 6.0584112 6.0584112 4.5 8 4.5 z M 17.25 6 A 0.75 0.75 0 0 0 17.25 7.5 A 0.75 0.75 0 0 0 17.25 6 z M 12 7 C 10.395834 7 9.0977407 7.6285667 8.2519531 8.5800781 C 7.4061656 9.5315896 7 10.770833 7 12 C 7 13.229167 7.4061656 14.46841 8.2519531 15.419922 C 9.0977407 16.371433 10.395834 17 12 17 C 13.604166 17 14.902259 16.371433 15.748047 15.419922 C 16.593834 14.46841 17 13.229167 17 12 C 17 10.770833 16.593834 9.5315896 15.748047 8.5800781 C 14.902259 7.6285667 13.604166 7 12 7 z M 12 8.5 C 13.229166 8.5 14.056075 8.9339333 14.626953 9.5761719 C 15.197832 10.21841 15.5 11.104167 15.5 12 C 15.5 12.895833 15.197832 13.78159 14.626953 14.423828 C 14.056075 15.066067 13.229166 15.5 12 15.5 C 10.770834 15.5 9.9439254 15.066067 9.3730469 14.423828 C 8.8021684 13.78159 8.5 12.895833 8.5 12 C 8.5 11.104167 8.8021684 10.21841 9.3730469 9.5761719 C 9.9439254 8.9339333 10.770834 8.5 12 8.5 z"></path>
+                        </svg>
+                        <span className="text-slate-100 text-xl group-hover:text-purple-300 transition-colors">https://www.instagram.com/czare.ek/</span>
+                    </a>
+                    <a href="https://www.instagram.com/czare.ek/" target="_blank" className="ml-1 flex gap-3 items-center group hover:translate-x-2 transition-transform">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 50 50" className="fill-slate-100 group-hover:fill-purple-300 transition-colors">
+                            <path d="M12 23.403V23.39 10.389L11.88 10.3h-.01L9.14 8.28C7.47 7.04 5.09 7.1 3.61 8.56 2.62 9.54 2 10.9 2 12.41v3.602L12 23.403zM38 23.39v.013l10-7.391V12.41c0-1.49-.6-2.85-1.58-3.83-1.46-1.457-3.765-1.628-5.424-.403L38.12 10.3 38 10.389V23.39zM14 24.868l10.406 7.692c.353.261.836.261 1.189 0L36 24.868V11.867L25 20l-11-8.133V24.868zM38 25.889V41c0 .552.448 1 1 1h6.5c1.381 0 2.5-1.119 2.5-2.5V18.497L38 25.889zM12 25.889L2 18.497V39.5C2 40.881 3.119 42 4.5 42H11c.552 0 1-.448 1-1V25.889z"></path>
+                        </svg>
+                        <span className="text-slate-100 text-xl group-hover:text-purple-300 transition-colors">kacperjaroszewski06@gmail.com</span>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
